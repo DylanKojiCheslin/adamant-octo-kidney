85681fd43e63c9de95744347896af9c0ab311b45
laxonmind <lucidpaper@gmail.com>
2015-08-02 16:46:21 -0600

    added sort by first or last name functions

--- easySearchin/both/collections/fullNamesCollection.js
+++ easySearchin/both/collections/fullNamesCollection.js
@@ -1,10 +1,29 @@
 FullNames = new Mongo.Collection("fullNames");
 
-FullNames.initEasySearch(['firstName', 'lastName'], {
-    'limit' : 20,
-    'use' : 'mongo-db'
-});
+EasySearch.createSearchIndex('fullNames', {
+  'collection': FullNames, // instanceof Meteor.Collection
+  'field': ['firstName', 'lastName'], // array of fields to be searchable
+  'limit': 10,
+  'use' : 'mongo-db',
+  'props': {
+    'sortBy' : 'first-name'
+  },
+  'sort': function() {
+    if (this.props.sortBy === 'first-name') {
+      return { 'firstName': 1 };
+    }  else if (this.props.sortBy === 'last-name') {
+      return { 'lastName': 1 };
+    }
+    // default by first name
+    return { 'score': -1 };
+  },
+  'query': function(searchString, opts) {
+    // Default query that will be used for the mongo-db selector
+    var query = EasySearch.getSearcher(this.use).defaultQuery(this, searchString);
 
+    return query;
+  }
+});
 //this is for the autoform package, and not relevent to easy search
 FullNames.attachSchema(new SimpleSchema({
   firstName: {
---
+++ easySearchin/client/templates/fullNames/searchFullNames/searchFullNames.js
@@ -0,0 +1,14 @@
+Template.searchFullNames.events({
+
+'change .sort-select': function(e) {
+  var instance = EasySearch.getComponentInstance({
+    index: 'fullNames'
+  });
+
+  EasySearch.changeProperty('fullNames', 'sortBy', $(e.target).children(':selected').data('sort'));
+  EasySearch.changeLimit('fullNames', 10);
+
+  instance.paginate(1);
+  instance.triggerSearch();
+}
+});
